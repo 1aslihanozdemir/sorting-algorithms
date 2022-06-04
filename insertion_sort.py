936bf76c850b8631aaffc0d155532ceecc74a1af
Aslıhan Özdemir <aslihanozdemir97@gmail.com>
2022-06-04 17:55:00 +0300

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,10 +1,10 @@
 #insertion sort
-list_0 = [4,7,3,8,0,2,1,6,5]
+my_list = [4,7,3,8,0,2,1,6,5]
 
-for i in range(1,len(list_0)):
+for i in range(1,len(my_list)):
     while i>0:
-        if list_0[i-1]>list_0[i]:
-           list_0[i-1],list_0[i]= list_0[i],list_0[i-1]
+        if my_list[i-1]>my_list[i]:
+           my_list[i-1],my_list[i]= my_list[i],my_list[i-1]
         i-=1
 
-print(list_0)
+print(my_list)
